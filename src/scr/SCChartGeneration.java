ed650d0343daa5e017f96ded999e131e12143d9e
Denis <denis@calltopower.de>
2009-12-31 04:52:29 +0100

    Aenderung des Namens in den Kommentaren

--- src/scr/SCChartGeneration.java
+++ src/scr/SCChartGeneration.java
@@ -1,25 +1,25 @@
 /**
- * Sugarscape Reproduction
+ * Sugarscape
  * Copyright 2009-2010 Denis M., Stefan H., Waldemar S.
  * 
  * Author: Denis M., Stefan H., Waldemar S.
- * Website: http://github.com/CallToPower/Sugarscape-Reproduction
+ * Website: http://github.com/CallToPower/Sugarscape
  * AG: Lecture "Regelbasierte Modelle" at the University of Osnabrueck (Germany)
  * 
- * The Sugarscape Reproduction is free Software:
+ * The Sugarscape is free Software:
  * You can redistribute it and/or modify it under the Terms of the
  * GNU General Public License
  * as published by the Free Software Foundation, either version 3 of the
  * License,
  * or (at your Option) any later Version.
  * 
- * The Sugarscape Reproduction Application is distributed WITHOUT ANY WARRANTY;
+ * The Sugarscape Application is distributed WITHOUT ANY WARRANTY;
  * without even the implied Warranty of MERCHANTABILITY or FITNESS FOR A
  * PARTICULAR PURPOSE.
  * See the GNU General Public License for more Details.
  * 
  * You should have received a copy of the GNU General Public License
- * along with the Sugarscape Reproduction Application.
+ * along with the Sugarscape Application.
  * If not, see <http://www.gnu.org/licenses/>.
  * 
  * File: scr/SCChartGeneration.java
